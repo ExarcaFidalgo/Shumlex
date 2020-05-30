fcb713dd04d1691f8653bdcc029805231a7fadb2
jorgealvarezfidalgo <uo258524@uniovi.es>
2020-05-30 14:20:33 +0200

    Expandiendo características a mostrar en grafo...

--- js/grafo.js
+++ js/grafo.js
@@ -96,7 +96,7 @@ function shExAGrafo(text) {
     for (let shape in source.shapes){
         irim.saveIri(shape, getID());
     }
-
+    console.log(source.shapes);
     for (let shape in source.shapes){
         if(source.shapes.hasOwnProperty(shape)) {
             let id = irim.findIri(shape);
@@ -104,12 +104,44 @@ function shExAGrafo(text) {
                 data: { id: id, name: irim.getPrefixedTermOfUri(shape) }
             });
 
-            let ats = checkExpression(source.shapes[shape].expression, id);
-            for(let i = 0; i < ats.length; i++) {
-                elements.push(ats[i]);
+            if( source.shapes[shape].nodeKind !== undefined) {
+                let idn = getID();
+                elements.push({ // node a
+                    data: { id: idn, name: source.shapes[shape].nodeKind }
+                });
+                elements.push({data: { id: getID(), name: "a", source: id,
+                        target: idn }});
             }
 
-            //data: { id: 'ab', source: 'a', target: 'b' }
+            if(source.shapes[shape].type === "ShapeAnd") {
+                let exprsForGen = [];
+                for (let i = 0; i < source.shapes[shape].shapeExprs.length; i++) {
+                    if(source.shapes[shape].shapeExprs[i].type === "ShapeRef") { // Herencia - :User :Person AND {}
+                        elements.push({data: { id: getID(), name: "a", source: id,
+                                target: irim.findIri(source.shapes[shape].shapeExprs[i].reference) }});
+                    }
+                    else if(source.shapes[shape].shapeExprs[i].type === "NodeConstraint") {
+                        let idn = getID();
+                        elements.push({ // node a
+                            data: { id: idn, name: source.shapes[shape].shapeExprs[i].nodeKind }
+                        });
+                        elements.push({data: { id: getID(), name: "a", source: id,
+                                target: idn }});
+                    }
+                    else {  //Conjunción de formas - :User { ... } AND { ... }
+                        let ats = checkExpression(source.shapes[shape].shapeExprs[i].expression, id);
+                        for(let i = 0; i < ats.length; i++) {
+                            elements.push(ats[i]);
+                        }
+                    }
+                }
+            } else {
+                let ats = checkExpression(source.shapes[shape].expression, id);
+                for(let i = 0; i < ats.length; i++) {
+                    elements.push(ats[i]);
+                }
+            }
+
         }
     }
     console.log(elements);
@@ -117,6 +149,7 @@ function shExAGrafo(text) {
 }
 
 function checkExpression(expr, father) {
+    console.log(expr);
     let attrs = [];
     if(!expr) {
         return attrs;
@@ -169,13 +202,107 @@ function determineTypeOfExpression(expr, father) {
             return attrs;
         }
         if(expr.valueExpr.values) {
-            let prvalues = "";
+            let idv = getID();
+            attrs.push({data: { id: idv, name: "values"}});
+            attrs.push({data: { id: getID(), name: irim.getPrefixedTermOfUri(name), source: father, target: idv }});
             for(let value in expr.valueExpr.values) {
-                prvalues += irim.getPrefixedTermOfUri(expr.valueExpr.values[value]) + "; ";
+                if(expr.valueExpr.values.hasOwnProperty(value)) {
+                if(expr.valueExpr.values[value].value !== undefined) {
+                    let ide = getID();
+                    attrs.push({data: { id: ide, name: expr.valueExpr.values[value].value}});
+                    attrs.push({data: { id: getID(), name: "", source: idv, target: ide }});
+                }
+                else if(expr.valueExpr.values[value].type === "LiteralStem") {
+                    let ide = getID();
+                    attrs.push({data: { id: ide, name: "&quot;" + expr.valueExpr.values[value].stem + "&quot;" + "~"}});
+                    attrs.push({data: { id: getID(), name: "", source: idv, target: ide }});
+                }
+                else if(expr.valueExpr.values[value].type === "IriStem") {
+                    let ide = getID();
+                    attrs.push({data: { id: ide, name: irim.getPrefixedTermOfUri(expr.valueExpr.values[value].stem) + "~"}});
+                    attrs.push({data: { id: getID(), name: "", source: idv, target: ide }});
+                }
+                else if(expr.valueExpr.values[value].type === "IriStemRange") {
+                    let ide = getID();
+                    if(expr.valueExpr.values[value].stem.type === "Wildcard") {
+                        attrs.push({data: { id: ide, name: "."}});
+                    }
+                    else {
+                        attrs.push({data: { id: ide, name: irim.getPrefixedTermOfUri(expr.valueExpr.values[value].stem) + "~ "}});
+                    }
+                    attrs.push({data: { id: getID(), name: "", source: idv, target: ide }});
+
+                    for(let k = 0; k < expr.valueExpr.values[value].exclusions.length; k++) {
+                        let excl = expr.valueExpr.values[value].exclusions[k];
+                        let idx = getID();
+                        if(excl.type === "IriStem") {
+                            attrs.push({data: { id: idx, name: irim.getPrefixedTermOfUri(excl.stem) + "~"}});
+                        }
+                        else {
+                            attrs.push({data: { id: idx, name: irim.getPrefixedTermOfUri(excl)}});
+                        }
+                        attrs.push({data: { id: getID(), name: "-", source: ide, target: idx }});
+                    }
+                }
+                else if(expr.valueExpr.values[value].type === "LiteralStemRange") {
+                    let ide = getID();
+                    if(expr.valueExpr.values[value].stem.type === "Wildcard") {
+                        attrs.push({data: { id: ide, name: "."}});
+                    }
+                    else {
+                        attrs.push({data: { id: ide, name: expr.valueExpr.values[value].stem + "~ "}});
+                    }
+                    attrs.push({data: { id: getID(), name: "", source: idv, target: ide }});
+
+                    for(let k = 0; k < expr.valueExpr.values[value].exclusions.length; k++) {
+                        let excl = expr.valueExpr.values[value].exclusions[k];
+                        let idx = getID();
+                        if(excl.type === "LiteralStem") {
+                            attrs.push({data: { id: idx, name: excl.stem + "~"}});
+                        }
+                        else {
+                            attrs.push({data: { id: idx, name: excl}});
+                        }
+                        attrs.push({data: { id: getID(), name: "-", source: ide, target: idx }});
+                    }
+                }
+                else if(expr.valueExpr.values[value].type === "Language") {
+                    let ide = getID();
+                    attrs.push({data: { id: ide, name: "@" + expr.valueExpr.values[value].languageTag}});
+                    attrs.push({data: { id: getID(), name: "", source: idv, target: ide }});
+                }
+                else if(expr.valueExpr.values[value].type === "LanguageStem") {
+                    let ide = getID();
+                    attrs.push({data: { id: ide, name: "@" + expr.valueExpr.values[value].stem + "~ "}});
+                    attrs.push({data: { id: getID(), name: "", source: idv, target: ide }});
+                }
+                else if(expr.valueExpr.values[value].type === "LanguageStemRange") {
+                    let ide = getID();
+                    if(expr.valueExpr.values[value].stem.type === "Wildcard") {
+                        attrs.push({data: { id: ide, name: "."}});
+                    }
+                    else {
+                        attrs.push({data: { id: ide, name: "@" + expr.valueExpr.values[value].stem + "~ "}});
+                    }
+                    attrs.push({data: { id: getID(), name: "", source: idv, target: ide }});
+
+                    for(let k = 0; k < expr.valueExpr.values[value].exclusions.length; k++) {
+                        let excl = expr.valueExpr.values[value].exclusions[k];
+                        let idx = getID();
+                        if(excl.type === "LanguageStem") {
+                            attrs.push({data: { id: idx, name: "@" + excl.stem + "~"}});
+                        }
+                        else {
+                            attrs.push({data: { id: idx, name: "@" + excl}});
+                        }
+                        attrs.push({data: { id: getID(), name: "-", source: ide, target: idx }});
+                    }
+                }
+
             }
-            let id = getID();
-            attrs.push({data: { id: id, name: prvalues}});
-            attrs.push({data: { id: getID(), name: irim.getPrefixedTermOfUri(name), source: father, target: id }});
+
+
+        }
             return attrs;
         }
         if(expr.valueExpr.nodeKind) {
@@ -186,12 +313,18 @@ function determineTypeOfExpression(expr, father) {
         }
         if(expr.valueExpr.datatype) {
             let id = getID();
+            let facets = checkFacets(expr.valueExpr, id);
+            attrs = attrs.concat(facets);
             attrs.push({data: { id: id, name: irim.getPrefixedTermOfUri(expr.valueExpr.datatype)}});
             attrs.push({data: { id: getID(), name: irim.getPrefixedTermOfUri(name), source: father, target: id }});
             return attrs;
         }
     }
     else if (expr.valueExpr.type === "ShapeRef") {
+        if(expr.predicate === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type") {
+            attrs.push({data: { id: getID(), name: "a", source: father, target: irim.findIri(expr.valueExpr.reference) }});
+            return attrs;
+        }
         attrs.push({data: { id: getID(), name: irim.getPrefixedTermOfUri(name), source: father, target: irim.findIri(expr.valueExpr.reference) }});
         return attrs;
     }
@@ -203,6 +336,14 @@ function determineTypeOfExpression(expr, father) {
         return attrs;
     }
     else if (expr.valueExpr.type === "ShapeAnd") {
+        let and = [];
+
+        for(let i = 0; i < expr.valueExpr.shapeExprs.length; i++) {
+            and = determineTypeOfExpression(expr.valueExpr.shapeExprs[i], father);
+            for(let j = 0; j < and.length; j++) {
+                attrs.push(and[j]);
+            }
+        }
         return attrs;
     }
 }
@@ -243,6 +384,78 @@ function cardinalityOf(attr) {
                 return " {" + lowerValue + ", " + upperValue + "}"
             }
     }
+}
+
+function checkFacets(vex, idn) {
+    let facets = [];
+    if(!vex) {
+        return facets;
+    }
+    if(vex.mininclusive) {
+        let idf = getID();
+        facets.push({ // node a
+            data: { id: idf, name: vex.mininclusive}
+        });
+        facets.push({data: { id: getID(), name: "MinInclusive", source: idn,
+                target: idf }});
+    }
+    if(vex.minexclusive) {
+        let idf = getID();
+        facets.push({ // node a
+            data: { id: idf, name: vex.minexclusive}
+        });
+        facets.push({data: { id: getID(), name: "MinExclusive", source: idn,
+                target: idf }});
+    }
+    if(vex.totaldigits) {
+        let idf = getID();
+        facets.push({ // node a
+            data: { id: idf, name: vex.totaldigits}
+        });
+        facets.push({data: { id: getID(), name: "TotalDigits", source: idn,
+                target: idf }});
+    }
+    if(vex.fractiondigits) {
+        let idf = getID();
+        facets.push({ // node a
+            data: { id: idf, name: vex.fractiondigits}
+        });
+        facets.push({data: { id: getID(), name: "FractionDigits", source: idn,
+                target: idf }});
+    }
+    if(vex.length) {
+        let idf = getID();
+        facets.push({ // node a
+            data: { id: idf, name: vex.length}
+        });
+        facets.push({data: { id: getID(), name: "Length", source: idn,
+                target: idf }});
+    }
+    if(vex.minlength) {
+        let idf = getID();
+        facets.push({ // node a
+            data: { id: idf, name: vex.minlength}
+        });
+        facets.push({data: { id: getID(), name: "MinLength", source: idn,
+                target: idf }});
+    }
+    if(vex.maxlength) {
+        let idf = getID();
+        facets.push({ // node a
+            data: { id: idf, name: vex.maxlength}
+        });
+        facets.push({data: { id: getID(), name: "MaxLength", source: idn,
+                target: idf }});
+    }
+    if(vex.pattern) {
+        let idf = getID();
+        facets.push({ // node a
+            data: { id: idf, name: "/" + vex.pattern + "/"}
+        });
+        facets.push({data: { id: getID(), name: "Pattern", source: idn,
+                target: idf }});
+    }
+    return facets;
 }
 
 exports.generarGrafo = generarGrafo;
