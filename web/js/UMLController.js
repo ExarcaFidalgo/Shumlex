47b468b6d58858271c5f176d0a8a05427d6b3c95
jorgealvarezfidalgo <uo258524@uniovi.es>
2020-12-15 20:26:27 +0100

    Descarga del diagrama UML en formato SVG

--- web/js/UMLController.js
+++ web/js/UMLController.js
@@ -8,6 +8,9 @@ const shumlex = require('shumlex');
  */
 function generarUML(xmi) {
     let puml = shumlex.crearDiagramaUML("uml", xmi);
+	let svg64 = shumlex.base64SVG("uml");
+	$("#dwnsvg-btn").attr("href", svg64);
+	$("#dwnsvg-btn").attr("download", `shumlex-class-diagram.svg`);
 }
 
 exports.generarUML = generarUML;
