b65bc83ddd704b201488a3de955f5c37ee7f06ec
jorgealvarezfidalgo <uo258524@uniovi.es>
2021-01-20 12:24:38 +0100

    Shumlex 0.4 y pantalla completa

--- web/js/UMLController.js
+++ web/js/UMLController.js
@@ -2,6 +2,8 @@ const $ = require('jquery');
 
 const shumlex = require('shumlex');
 
+let isFullscreen = false;
+
 /**
  * Asigna a la imagen reservada el enlace del mermaid generado
  * @param xmi   XMI empleado para generar UML
@@ -12,5 +14,28 @@ function generarUML(xmi) {
 	$("#dwnsvg-btn").attr("href", svg64);
 	$("#dwnsvg-btn").attr("download", `shumlex-class-diagram.svg`);
 }
+
+$("#fullscreen").click(fullscreen);
+$("#fullscreen2").click(fullscreen);
+
+function fullscreen() {
+		if(!isFullscreen) {
+			$("#umlcontainer").addClass("fullscreen");
+			$("#umlcontainer").removeClass("displaydiv");
+			$("#uml").css("max-height", "91%");
+			$("#umlcontainer").css("width", "100%");
+			$("#fullscreen2").css("display", "inherit");
+			$("body").css("overflow", "hidden");
+			isFullscreen = true;
+		} else {
+			$("#umlcontainer").removeClass("fullscreen");
+			$("#umlcontainer").addClass("displaydiv");
+			$("#uml").css("max-height", "500px");
+			$("#umlcontainer").css("width", "44.6%");
+			$("#fullscreen2").css("display", "none");
+			$("body").css("overflow", "");
+			isFullscreen = false;
+		}
+	}
 
 exports.generarUML = generarUML;
